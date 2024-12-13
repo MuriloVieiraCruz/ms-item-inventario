f6c609e150329f5f29d1a810a6f8e00102ca8b09
Murilo Vieira <murilo12super@gmail.com>
2024-12-13 14:27:54 -0300

    Inicio do projeto

---
+++ src/main/java/com/murilo/TesteFarolShopping/TesteFarolShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.murilo.TesteFarolShopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteFarolShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteFarolShoppingApplication.class, args);
+	}
+
+}
